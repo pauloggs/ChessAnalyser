712ff294a88aa0d21b8bc41c456e9e4f71a1d66f
pauloggs <pshodgkinson@googlemail.com>
2024-09-08 00:56:14 +0100

    Removed boiler plate code.7

---
+++ Analyser/Controllers/Analyser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Analyser.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Analyser : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+    }
+}
