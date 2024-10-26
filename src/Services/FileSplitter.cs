95ac55df3ba6b8cb5af9129e54943921ce11ac35
pauloggs <pshodgkinson@googlemail.com>
2024-10-26 22:24:48 +0100

    Stub for splitter

---
+++ src/Services/FileSplitter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IFileSplitter
+    {
+        void GetRawGamesFromPgnFile(string pgnFile);
+    }
+
+    public class FileSplitter : IFileSplitter
+    {
+        public void GetRawGamesFromPgnFile(string pgnFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
