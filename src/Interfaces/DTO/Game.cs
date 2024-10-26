d056a2fc85f7339c04d9a81f05088f14052a847a
pauloggs <pshodgkinson@googlemail.com>
2024-10-26 20:33:59 +0100

    Add repo

---
+++ src/Interfaces/DTO/Game.cs
@@ -0,0 +1,7 @@
+﻿namespace Interfaces.DTO
+{
+    public class Game
+    {
+        public required string Name { get; set; }
+    }
+}
---
+++ src/Repositories/ChessRepository.cs
@@ -0,0 +1,30 @@
+﻿using Interfaces.DTO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Repositories
+{
+    public interface IChessRepository
+    {
+        IConfiguration Configuration { get; }
+
+        List<Game> GetGames();
+    }
+
+    public class ChessRepository
+    {
+        private readonly IConfiguration _config;
+
+        public ChessRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection GetOpenConnection()
+        {
+            var cs = new SqlConnection(_config.GetConnectionString("ChessConnection"));
+            cs.Open();
+            return cs;
+        }
+    }
+}
